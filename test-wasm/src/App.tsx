ff1039dc0569891aecdaf15148688ca4e3755e52
Toumi-Elyes <elyes.toumi@epitech.eu>
2023-02-02 18:33:08 +0100

    fix(canva): elements coming back when cleaning and trailing lines for arcs and circles

--- test-wasm/src/App.tsx
+++ test-wasm/src/App.tsx
@@ -1,96 +1,129 @@
 import React from "react";
 import "./App.css";
 import * as wasm from "poc-test-wasm";
+import styled from 'styled-components';
 
 wasm.default();
 
-const canva_ctx = wasm.start();
-
-const draw = () =>  {
-  wasm.draw_face(canva_ctx);
-}
-
-let x = 50;
-let y = 50;
+const canvaCtx = wasm.start("canvas");
 
 function App() {
 
-  const [text, setText] = React.useState("");
-  const [x, setX] = React.useState(50);
-  const [y, setY] = React.useState(50);
+  const [canvaId, setCanvaId] = React.useState<string>("canvas");
+  const [text, setText] = React.useState<string>("");
+  const [x, setX] = React.useState<number>(50);
+  const [y, setY] = React.useState<number>(50);
 
 
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
     setText(event.currentTarget.value);
   };
+
   const handleXPos = (event: React.FormEvent<HTMLInputElement>) => {
     setX(parseInt(event.currentTarget.value));
   };
+
   const handleYPos = (event: React.FormEvent<HTMLInputElement>) => {
     setY(parseInt(event.currentTarget.value));
   };
 
   const drawline = () =>  {
-    wasm.draw_line(canva_ctx, x, y);
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_line(canvaCtx, x, y);
+    wasm.stop_drawing(canvaCtx);
   }
 
-  const drawtext = () =>  {
-    wasm.draw_text(canva_ctx, text, x, y, 300);
+  const drawtext = () => {
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_text(canvaCtx, text, x, y, 500);
+    wasm.fill(canvaCtx);
+    wasm.stop_drawing(canvaCtx);
   }
 
-  const drawrect = () =>  {
-    wasm.draw_rect(canva_ctx, x, y, 20, 30, false);
+  const drawrect = () => {
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_rect(canvaCtx, x, y, 20, 30);
+    wasm.stroke(canvaCtx);
   }
 
-  const setCyan = () =>  {
-    wasm.set_color(canva_ctx, "cyan");
+  const setCyan = () => {
+    wasm.set_color(canvaCtx, "cyan");
   }
 
-  const setRed = () =>  {
-    wasm.set_color(canva_ctx, "red");
+  const setRed = () => {
+    wasm.set_color(canvaCtx, "red");
   }
 
-  const setGreen = () =>  {
-    wasm.set_color(canva_ctx, "green");
+  const setGreen = () => {
+    wasm.set_color(canvaCtx, "green");
   }
 
-  const drawCircle = () =>  {
-    wasm.draw_circle(canva_ctx, x, y, 50);
+  const clearCanva = () => {
+    wasm.clear_canva(canvaCtx, 300, 300);
   }
 
-  const clearCanva = () =>  {
-    setX(50);
-    setY(50);
-    wasm.clear_canva(canva_ctx, 500, 500);
+  const drawCircle = () => {
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_circle(canvaCtx, x, y, 50);
+    wasm.stop_drawing(canvaCtx);
+
   }
 
-  const drawArc = () =>  {
-    wasm.draw_arc(canva_ctx, x, y, 50, 1, 2);
+
+  const drawArc = () => {
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_arc(canvaCtx, x, y, 50, 1, 2);
+    wasm.fill(canvaCtx);
+    wasm.stop_drawing(canvaCtx);
+  }
+
+  const drawBoxCircle = () => {
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_circle(canvaCtx, x, y, 50);
+    wasm.stop_drawing(canvaCtx);
+    wasm.sleep_milliseconds(BigInt(500));
+    wasm.start_drawing(canvaCtx);
+    wasm.draw_rect(canvaCtx, x, y, 50, 60);
+    wasm.stop_drawing(canvaCtx);
   }
 
 
   return (
-    <div className="App">
-      <input onChange={handleChange} placeholder="text"/>
-      <input onChange={handleXPos} placeholder="x pos"/>
-      <input onChange={handleYPos} placeholder="y pos"/>
-      <button onClick={draw}>draw face</button>
-      <button onClick={drawline}>line</button>
-      <button onClick={drawtext}>text</button>
-      <button onClick={drawrect}>rect</button>
-      <button onClick={setCyan}>cyan</button>
-      <button onClick={setRed}>red</button>
-      <button onClick={setGreen}>green</button>
-      <button onClick={drawCircle}>circle</button>
-      <button onClick={clearCanva}>clear</button>
-      <button onClick={drawArc}>arc</button>
-      <div>
-        <canvas id="canvas" width="300" height="300">
-        </canvas>
-      </div>
-    </div>
+    <Div className="App">
+      <Wrapper>
+        <Div>
+          <input onChange={handleChange} placeholder="text"/>
+          <input onChange={handleXPos} placeholder="x pos"/>
+          <input onChange={handleYPos} placeholder="y pos"/>
+          <button onClick={drawtext}>text</button>
+          <button onClick={drawrect}>rect</button>
+          <button onClick={setCyan}>cyan</button>
+          <button onClick={setRed}>red</button>
+          <button onClick={setGreen}>green</button>
+          <button onClick={drawCircle}>circle</button>
+          <button onClick={drawArc}>arc</button>
+          <button onClick={clearCanva}>clear</button>
+          <button onClick={drawBoxCircle}>boxCircle</button>
+        </Div>
+      </Wrapper>
+      <Div>
+        <Canvas id={canvaId} width="300" height="300">
+        </Canvas>
+      </Div>
+    </Div>
   );
 }
 
+const Div = styled.div`
+`
+
+const Canvas = styled.canvas`
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
 
 export default App;
--- wasm-packages/poc-test-wasm/src/lib.rs
+++ wasm-packages/poc-test-wasm/src/lib.rs
@@ -1,10 +1,9 @@
 // use std::convert::TryInto;
 use std::f64;
-use web_sys::Element;
+use std::{thread, time};
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use std::collections::HashMap;
-use web_sys::{HtmlElement, Document, HtmlCanvasElement};
+use web_sys::{HtmlCanvasElement};
 
 
 
@@ -25,47 +24,9 @@ pub fn greet() {
 }
 
 #[wasm_bindgen]
-pub fn display_blue_square() -> Result<(), JsValue> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    let svg = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?;
-    svg.set_attribute("width", "100")?;
-    svg.set_attribute("hight", "100")?;
-    svg.set_attribute("viewBox", "0 0 100 100")?;
-
-    let square = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "rect")?;
-    square.set_attribute("width", "100")?;
-    square.set_attribute("height", "100")?;
-    square.set_attribute("fill", "blue")?;
-    svg.append_child(&square)?;
-    body.append_child(&svg)?;
-
-    Ok(())
-}
-
-fn _attributes_to_hashmap(attributes_str: &str) -> HashMap<String, &str> {
-    let split = attributes_str.split(";");
-    let mut attributes_map = HashMap::new();
-
-    let attributes_list: Vec<&str> = split.collect();
-
-    for content in attributes_list {
-        let key_value_list = content.split("=");
-
-        let key_value: Vec<&str> = key_value_list.collect();
-
-        attributes_map.insert(String::from(key_value[0]), key_value[1]);
-    }
-    return attributes_map;
-}
-
-
-#[wasm_bindgen]
-pub fn start() -> Result<web_sys::CanvasRenderingContext2d, JsValue> {
+pub fn start(id: &str) -> Result<web_sys::CanvasRenderingContext2d, JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas = document.get_element_by_id(id).unwrap();
     let canvas: HtmlCanvasElement = canvas
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
@@ -78,68 +39,25 @@ pub fn start() -> Result<web_sys::CanvasRenderingContext2d, JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.begin_path();
-
     return Ok(context);
 }
 
 #[wasm_bindgen]
-pub fn draw_face(ctx: web_sys::CanvasRenderingContext2d) {
-    // Draw the outer circle.
-    ctx
-    .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-    .unwrap();
-
-    // Draw the mouth.
-    ctx.move_to(110.0, 75.0);
-    ctx.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    ctx.move_to(65.0, 65.0);
-    ctx
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    ctx.move_to(95.0, 65.0);
-    ctx
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    ctx.stroke();
-}
-
-
-#[wasm_bindgen]
 pub fn draw_line(ctx: web_sys::CanvasRenderingContext2d, x: f64, y: f64) {
     ctx.line_to(x, y);
-
-    ctx.stroke();
 }
 
 #[wasm_bindgen]
-pub fn draw_rect(ctx: web_sys::CanvasRenderingContext2d, x: f64, y: f64, w: f64, h: f64, filled: bool, c: Option<String>) {
+pub fn draw_rect(ctx: web_sys::CanvasRenderingContext2d, x: f64, y: f64, w: f64, h: f64, c: Option<String>) {
     let _color: String = c.unwrap_or(String::from("black"));
 
     ctx.rect(x, y, w, h);
-    if filled {
-        // ctx.fillStyle = color;
-        ctx.fill();
-    } else {
-        // ctx.strokeStyle = color;
-        ctx.stroke();
-    }
 }
 
-// #[wasm_bindgen]
-// pub fn draw_image(ctx: web_sys::CanvasRenderingContext2d, img_id: &str, x: f64, y: f64) -> Result<(), JsValue> {
-//     let document = web_sys::window().unwrap().document().unwrap();
-//     let image = document.get_element_by_id(img_id).unwrap();
-
-//     ctx.draw_image_with_html_canvas_element(image as &HtmlCanvasElement, x, y).unwrap();
-
-//     return Ok(());
-// }
+#[wasm_bindgen]
+pub fn sleep_milliseconds(duration: u64) {
+    thread::sleep(time::Duration::from_secs(duration));
+}
 
 #[wasm_bindgen]
 pub fn draw_text(ctx: web_sys::CanvasRenderingContext2d, text: &str, x: f64, y: f64, w: f64) -> Result<(), JsValue> {
@@ -153,6 +71,7 @@ pub fn set_color(ctx: web_sys::CanvasRenderingContext2d, text: &str) {
 
 #[wasm_bindgen]
 pub fn clear_canva(ctx: web_sys::CanvasRenderingContext2d, w: f64, h: f64) {
+    ctx.move_to(0.0, 0.0);
     ctx.clear_rect(0.0, 0.0, w, h);
 }
 
@@ -165,16 +84,37 @@ pub fn clear_rect(ctx: web_sys::CanvasRenderingContext2d, x: f64, y: f64, w: f64
 pub fn draw_circle(ctx: web_sys::CanvasRenderingContext2d, x: f64, y: f64, s: f64) -> Result<(), JsValue> {
     let arc_res = ctx.arc(x, y, s, 0.0, 2.0 * std::f64::consts::PI);
 
-    ctx.stroke();
-
     return arc_res;
+}
+
+#[wasm_bindgen]
+pub fn stroke(ctx: web_sys::CanvasRenderingContext2d) {
+    ctx.stroke();
+}
+
+#[wasm_bindgen]
+pub fn fill(ctx: web_sys::CanvasRenderingContext2d) {
+    ctx.fill();
 }
 
 #[wasm_bindgen]
 pub fn draw_arc(ctx: web_sys::CanvasRenderingContext2d, x: f64, y: f64, s: f64, start_angle: f64, end_angle: f64) -> Result<(), JsValue> {
     let arc_res = ctx.arc(x, y, s, start_angle, end_angle);
 
-    ctx.stroke();
-
     return arc_res;
 }
+
+#[wasm_bindgen]
+pub fn start_drawing(ctx: web_sys::CanvasRenderingContext2d) {
+    ctx.begin_path();
+}
+
+#[wasm_bindgen]
+pub fn stop_drawing(ctx: web_sys::CanvasRenderingContext2d) {
+    ctx.close_path();
+}
+
+// ! refacto code to add classe
+// TODO add color
+// TODO remove extra line
+// TODO check why circle get filled when its not supposed to do
